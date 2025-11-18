3465340068cb161b5dbfe550b63f7529c71eb0da
Jairo <jairo@escasan.com>
2025-11-17 23:05:04 -0600

    Actualizar configuraciÃ³n de auth.php

--- backend/config/auth.php
+++ backend/config/auth.php
@@ -60,16 +60,12 @@ return [
     */
 
     'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
-        ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+    'users' => [
+        'driver' => 'eloquent',
+        // Cambia esta línea:
+        'model' => App\Models\Usuario::class,
     ],
+],
 
     /*
     |--------------------------------------------------------------------------
